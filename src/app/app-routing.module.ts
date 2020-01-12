c04785ca34cbdab0742d74e5ec521c6f9bb6f62d
thomas maestas <thomasm1.maestas@gmail.com>
2020-01-12 15:57:29 -0500

    added login component;

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; 
-
-import { AppComponent } from './app.component';
+ 
 import { DoggydashComponent } from './components/doggydash/doggydash.component';
 import { ClientsComponent } from './components/clients/clients.component';
 import { EmployeesComponent } from './components/employees/employees.component';
-import { FrontpageComponent } from './layout/frontpage/frontpage.component';
+import { LoginComponent } from './components/login/login.component'; 
 
 
 const routes: Routes = [
    
-  { path: '', component: FrontpageComponent },
+  { path: '', component: LoginComponent },
   { path: 'pets', component: DoggydashComponent },  
   { path: 'admin', component: EmployeesComponent },
   { path: 'clients', component: ClientsComponent }, 
