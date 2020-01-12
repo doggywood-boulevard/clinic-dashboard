20579f39b23413e81e541bb0c90bcc6e43eaf120
Thomas M. Maestas <thomasm1.maestas@gmail.com>
2020-01-12 17:25:29 -0500

    Thomas branch (#11)
    * plugged in Davis' logo
    
    * refactored folder groupings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
 
 import { EmployeesService } from './services/employees.service';
 import { ClientsService } from './services/clients.service';  
-import { ClientsComponent } from './components/clients/clients.component';
+import { ClientsComponent } from './components/cli/clients/clients.component';
 import { EmployeesComponent } from './components/employees/employees.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DoggynavComponent } from './layout/doggynav/doggynav.component';
@@ -24,21 +24,21 @@ import { DoggydashComponent } from './components/doggydash/doggydash.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
-import { LoginComponent } from './components/login/login.component';
-import { CreateClientComponent } from './components/create-client/create-client.component';
-import { CliProfileComponent } from './components/cli-profile/cli-profile.component';
-import { VetCliProfileComponent } from './components/vet-cli-profile/vet-cli-profile.component';
+import { LoginComponent } from './components/logins/login/login.component';
+import { CreateClientComponent } from './components/cli/create-client/create-client.component';
+import { CliProfileComponent } from './components/landing/cli-profile/cli-profile.component';
+import { VetCliProfileComponent } from './components/vet/vet-cli-profile/vet-cli-profile.component';
 import { ApptComponent } from './components/appt/appt.component';
 import { ApptCreateComponent } from './components/appt-create/appt-create.component';
-import { EmpVetLoginComponent } from './components/emp-vet-login/emp-vet-login.component';
-import { EmpRecLoginComponent } from './components/emp-rec-login/emp-rec-login.component';
-import { CliAnimalProfileComponent } from './components/cli-animal-profile/cli-animal-profile.component';
-import { VetAnimalProfileComponent } from './components/vet-animal-profile/vet-animal-profile.component';
+import { EmpVetLoginComponent } from './components/logins/emp-vet-login/emp-vet-login.component';
+import { EmpRecLoginComponent } from './components/logins/emp-rec-login/emp-rec-login.component';
+import { CliAnimalProfileComponent } from './components/cli/cli-animal-profile/cli-animal-profile.component';
+import { VetAnimalProfileComponent } from './components/vet/vet-animal-profile/vet-animal-profile.component';
 import { AnimalCreateComponent } from './components/animal-create/animal-create.component';
-import { VetLandingComponent } from './components/vet-landing/vet-landing.component';
-import { VetPetRecordComponent } from './components/vet-pet-record/vet-pet-record.component';
-import { CliPetRecordComponent } from './components/cli-pet-record/cli-pet-record.component';
-import { RecLandingComponent } from './components/rec-landing/rec-landing.component'; 
+import { VetLandingComponent } from './components/landing/vet-landing/vet-landing.component';
+import { VetPetRecordComponent } from './components/vet/vet-pet-record/vet-pet-record.component';
+import { CliPetRecordComponent } from './components/cli/cli-pet-record/cli-pet-record.component';
+import { RecLandingComponent } from './components/landing/rec-landing/rec-landing.component'; 
 
 @NgModule({
   declarations: [
