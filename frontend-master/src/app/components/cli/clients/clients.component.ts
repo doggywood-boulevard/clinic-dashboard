f6953688a92fc9c8f15aab907279c25710cd2734
flubbid <brendanwilson1@gmail.com>
2020-01-14 14:47:48 -0500

    updated client profile page

--- frontend-master/src/app/components/cli/clients/clients.component.ts
+++ frontend-master/src/app/components/cli/clients/clients.component.ts
@@ -7,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientsComponent implements OnInit {
 
-  subtitle ='Clients';
+
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
