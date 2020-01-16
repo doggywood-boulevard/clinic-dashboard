8bc90a409daf5dfce42f408ebdfbc99f78cb1603
thomas maestas <thomasm1.maestas@gmail.com>
2020-01-16 17:55:05 -0500

    doggywood with bootstrap do-over w/routing navigation

---
+++ doggywood/src/app/components/logins/emp-vet-login/emp-vet-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-emp-vet-login',
+  templateUrl: './emp-vet-login.component.html',
+  styleUrls: ['./emp-vet-login.component.css']
+})
+export class EmpVetLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ doggywood/src/app/components/pet-create/pet-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-create',
+  templateUrl: './pet-create.component.html',
+  styleUrls: ['./pet-create.component.css']
+})
+export class PetCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
