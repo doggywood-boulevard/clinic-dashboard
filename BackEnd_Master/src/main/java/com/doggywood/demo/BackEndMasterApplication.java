e1484ad9957b81cb7708ee39d22d977d160d4099
flubbid <brendanwilson1@gmail.com>
2020-01-16 20:23:44 -0500

    adds the Spring Starter Project

---
+++ BackEnd_Master/src/main/java/com/doggywood/demo/BackEndMasterApplication.java
@@ -0,0 +1,13 @@
+package com.doggywood.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndMasterApplication.class, args);
+	}
+
+}
