0600f195ee5e79663b353399e01be26c3a7103fa
flubbid <49663994+flubbid@users.noreply.github.com>
2020-01-17 08:55:51 -0500

    Brendan branch (#39)
    * adds Pet Model
    
    * adds Pet Repository
    
    * adds pet service interface
    
    * updates C_id to cid to work with CRUD Repo
    
    * adds get pets by CID method
    
    * adds pet by CID method
    
    * adds Pet Service Impl
    
    * adds Pet Controller
    
    * adds references/annotations
    
    * updates method name to proper entity id
    
    * updates c_id methods name
    
    * fixes typo in description

--- BackEnd_Master/src/main/java/com/doggywood/demo/BackEndMasterApplication.java
+++ BackEnd_Master/src/main/java/com/doggywood/demo/BackEndMasterApplication.java
@@ -2,8 +2,14 @@ package com.doggywood.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.doggywood.repositories")
+@ComponentScan("com.doggywood")
+@EntityScan("com.doggywood.entities")
 public class BackEndMasterApplication {
 
 	public static void main(String[] args) {
---
+++ BackEnd_Master/src/main/java/com/doggywood/entities/Pet.java
@@ -0,0 +1,137 @@
+package com.doggywood.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Pet {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "P_ID")
+	private int id;
+	
+	@Column(name = "cId")
+	private int cId;
+	
+	@Column(name = "P_NAME")
+	private String petName;
+	
+	@Column(name = "DOB")
+	private String birthDate;
+	
+	@Column(name = "P_WEIGHT")
+	private int weight;
+	
+	@Column(name = "P_TYPE")
+	private int type;
+	
+	@Column(name = "P_BREED")
+	private String breed;
+	
+	@Column(name = "P_description")
+	private String description;
+
+	public Pet(int id, int cId, String petName, String birthDate, int weight, int type, String breed,
+			String description) {
+		super();
+		this.id = id;
+		this.cId = cId;
+		this.petName = petName;
+		this.birthDate = birthDate;
+		this.weight = weight;
+		this.type = type;
+		this.breed = breed;
+		this.description = description;
+	}
+
+	public Pet(int cId, String petName, String birthDate, int weight, int type, String breed, String description) {
+		super();
+		this.cId = cId;
+		this.petName = petName;
+		this.birthDate = birthDate;
+		this.weight = weight;
+		this.type = type;
+		this.breed = breed;
+		this.description = description;
+	}
+
+	public Pet() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getcId() {
+		return cId;
+	}
+
+	public void setcId(int cId) {
+		this.cId = cId;
+	}
+
+	public String getPetName() {
+		return petName;
+	}
+
+	public void setPetName(String petName) {
+		this.petName = petName;
+	}
+
+	public String getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	public String getBreed() {
+		return breed;
+	}
+
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+
+	public String getdescription() {
+		return this.description;
+	}
+
+	public void setdescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public String toString() {
+		return "Pet [id=" + id + ", cId=" + cId + ", petName=" + petName + ", birthDate=" + birthDate + ", weight="
+				+ weight + ", type=" + type + ", breed=" + breed + ", description=" + description + "]";
+	}
+	
+	
+	
+}
