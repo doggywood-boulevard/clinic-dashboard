dd16989c2c80e62a72c9745a60d6e3398bdf2a1a
flubbid <brendanwilson1@gmail.com>
2020-01-16 19:29:35 -0500

    adds spring starter project

---
+++ Doggywood_Veterinary/src/main/java/com/doggywood/demo/DoggywoodVeterinaryApplication.java
@@ -0,0 +1,13 @@
+package com.doggywood.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoggywoodVeterinaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoggywoodVeterinaryApplication.class, args);
+	}
+
+}
