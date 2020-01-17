4718cd60f14947124a7f74133ea18308c392a198
flubbid <brendanwilson1@gmail.com>
2020-01-16 22:39:44 -0500

    adds get pets by CID method

--- BackEnd_Master/src/main/java/com/doggywood/repositories/PetRepository.java
+++ BackEnd_Master/src/main/java/com/doggywood/repositories/PetRepository.java
@@ -1,5 +1,7 @@
 package com.doggywood.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,6 @@ import com.doggywood.entities.Pet;
 @Repository
 public interface PetRepository extends CrudRepository<Pet, Integer> {
 
-	
+	List<Pet> findByCId(int c_id);
 	
 }
