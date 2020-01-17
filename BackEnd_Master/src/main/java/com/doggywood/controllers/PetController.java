9e627f206d5a07d9966a8c1349616c1a7658512f
flubbid <brendanwilson1@gmail.com>
2020-01-17 18:20:31 -0500

    updates for Angular CORS

--- BackEnd_Master/src/main/java/com/doggywood/controllers/PetController.java
+++ BackEnd_Master/src/main/java/com/doggywood/controllers/PetController.java
@@ -3,10 +3,7 @@ package com.doggywood.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
-=======
 import org.springframework.web.bind.annotation.CrossOrigin;
->>>>>>> d6ebe624ebb154d332fac8a41a792960faf03814
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,51 +23,37 @@ public class PetController {
 	PetService ps;
 	
 	@RequestMapping(value = "/pets", method = RequestMethod.POST, consumes = "application/json")
-<<<<<<< HEAD
-=======
 //	@CrossOrigin(origins = "http://localhost:8080")
 	@CrossOrigin(origins = "*")
->>>>>>> d6ebe624ebb154d332fac8a41a792960faf03814
 	public Pet createPet(@RequestBody Pet pet) {
 		return ps.createPet(pet);
 	}
 	
 	@GetMapping(value = "/pets/{id}")
-<<<<<<< HEAD
-=======
+
 //	@CrossOrigin(origins = "http://localhost:8080")
 	@CrossOrigin(origins = "*")
->>>>>>> d6ebe624ebb154d332fac8a41a792960faf03814
 	public Pet getPetById(@PathVariable("id") int id) {
 		return ps.getPetById(id);
 	}
 
 	@GetMapping(value = "/pets")
-<<<<<<< HEAD
-=======
 //	@CrossOrigin(origins = "http://localhost:8080")
 	@CrossOrigin(origins = "*")
->>>>>>> d6ebe624ebb154d332fac8a41a792960faf03814
 	public List<Pet> getAllPets() {
 		return ps.getAllPets();
 	}
 
 	@PutMapping(value = "/pets", consumes = "application/json")
-<<<<<<< HEAD
-=======
 //	@CrossOrigin(origins = "http://localhost:8080")
 	@CrossOrigin(origins = "*")
->>>>>>> d6ebe624ebb154d332fac8a41a792960faf03814
 	public Pet updatesPet(Pet change) {
 		return ps.updatesPet(change);
 	}
 
 	@DeleteMapping(value = "/pets", consumes = "application/json")
-<<<<<<< HEAD
-=======
 //	@CrossOrigin(origins = "http://localhost:8080")
 	@CrossOrigin(origins = "*")
->>>>>>> d6ebe624ebb154d332fac8a41a792960faf03814
 	public boolean deletePet(Pet pet) {
 		try {
 			ps.deletePet(pet);
@@ -84,11 +67,8 @@ public class PetController {
 	
 	//Come back to this to provide restfull route
 	@GetMapping(value = "pets/customer/{cid}")
-<<<<<<< HEAD
-=======
 //	@CrossOrigin(origins = "http://localhost:8080")
 	@CrossOrigin(origins = "*")
->>>>>>> d6ebe624ebb154d332fac8a41a792960faf03814
 	public List<Pet> getAllPetsByCustomer(int c_id) {
 		return ps.getAllPetsByCustomer(c_id);
 	}
