284e7dee0bbaa643e9c170449f5cec6336028057
flubbid <brendanwilson1@gmail.com>
2020-01-16 21:41:29 -0500

    adds Pet Model

---
+++ BackEnd_Master/src/main/java/com/doggywood/entities/Pet.java
@@ -0,0 +1,137 @@
+package com.doggywood.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Pet {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "P_ID")
+	private int id;
+	
+	@Column(name = "C_ID")
+	private int c_id;
+	
+	@Column(name = "P_NAME")
+	private String petName;
+	
+	@Column(name = "DOB")
+	private String birthDate;
+	
+	@Column(name = "P_WEIGHT")
+	private int weight;
+	
+	@Column(name = "P_TYPE")
+	private int type;
+	
+	@Column(name = "P_BREED")
+	private String breed;
+	
+	@Column(name = "P_DESCRIPTION")
+	private String Description;
+
+	public Pet(int id, int c_id, String petName, String birthDate, int weight, int type, String breed,
+			String description) {
+		super();
+		this.id = id;
+		this.c_id = c_id;
+		this.petName = petName;
+		this.birthDate = birthDate;
+		this.weight = weight;
+		this.type = type;
+		this.breed = breed;
+		Description = description;
+	}
+
+	public Pet(int c_id, String petName, String birthDate, int weight, int type, String breed, String description) {
+		super();
+		this.c_id = c_id;
+		this.petName = petName;
+		this.birthDate = birthDate;
+		this.weight = weight;
+		this.type = type;
+		this.breed = breed;
+		Description = description;
+	}
+
+	public Pet() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getC_id() {
+		return c_id;
+	}
+
+	public void setC_id(int c_id) {
+		this.c_id = c_id;
+	}
+
+	public String getPetName() {
+		return petName;
+	}
+
+	public void setPetName(String petName) {
+		this.petName = petName;
+	}
+
+	public String getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	public String getBreed() {
+		return breed;
+	}
+
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+
+	public String getDescription() {
+		return Description;
+	}
+
+	public void setDescription(String description) {
+		Description = description;
+	}
+
+	@Override
+	public String toString() {
+		return "Pet [id=" + id + ", c_id=" + c_id + ", petName=" + petName + ", birthDate=" + birthDate + ", weight="
+				+ weight + ", type=" + type + ", breed=" + breed + ", Description=" + Description + "]";
+	}
+	
+	
+	
+}
