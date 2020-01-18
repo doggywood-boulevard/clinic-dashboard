f785c2f1ece3c248df0a6bd0e8f567bcf5baae17
thomas maestas <thomasm1.maestas@gmail.com>
2020-01-18 02:38:30 -0500

    doggywood pets

--- doggywood/src/app/components/pet-create/pet-create.component.ts
+++ doggywood/src/app/components/pet-create/pet-create.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router, ActivatedRoute, Navigation } from '@angular/router';
+import { FormBuilder, FormGroup, FormArray, Validators, NgForm } from '@angular/forms';
 import { PetsService } from '../../services/pets.service';
-
+import { tap, first } from 'rxjs/operators';
+import { PetType } from '../../models/petType'
+import { Pet } from 'src/app/models/petModel';
 
 @Component({
   selector: 'app-pet-create',
@@ -9,26 +13,109 @@ import { PetsService } from '../../services/pets.service';
 })
 export class PetCreateComponent implements OnInit {
 
-  constructor(private aniService: PetsService) { }
+  // regForm: FormGroup;
+  // form-state
+  loading = false;
+  success = false;
+  panelTitle: string;
+  pet: Pet;
+  petGroups: PetType[] = [
+    { type: 1, typeName: 'Dog' },
+    { type: 2, typeName: 'Cat' },
+    { type: 3, typeName: 'Parrot' },
+    { type: 4, typeName: 'Ferret' },
+    { type: 5, typeName: 'Other' },
+  ];
+  dateOfBirth: Date = new Date(2020, 1, 30);
+   datePickerConfig:any;
+//  @ViewChild('petForm', opts: {
+//         pet: any;
+//         static: boolean;
+//     })
+  public createPetForm: NgForm;
   public petsList = [];
   public petsLocalDBList = [];
 
+  constructor(private aniService: PetsService, private router: Router,
+    private activatedRoute: ActivatedRoute) {
+      this.datePickerConfig = Object.assign({}, 
+      {
+        containerClass: 'theme-dark-blue',
+        showWeekNumbers: true,
+        // minDate: new Date(2000, 0,1), // pets < 20 years?
+        dateInputFormat: 'yyyy-MM-dd'  
+        });
+    }
+
   ngOnInit() {    // on page load here  
     this.aniService.getPets()
-      .subscribe(data => this.petsList = data); 
-// pets_url: string = 'http://localhost:8080/pets';
-// [{
-// 	"id": 2,
-// 	"cId": 1,
-// 	"petName": "Whiskers",
-// 	"birthDate": "2017-08-01 00:00:00.0",
-// 	"weight": 69,
-// 	"type": 2,
-// 	"breed": "English Bdog",
-// 	"description": null
-// }]
+      .subscribe(data => this.petsList = data);
 
     this.aniService.getPetsLocal()
       .subscribe(data => this.petsLocalDBList = data);
-  } 
+    this.activatedRoute.paramMap.subscribe(parameterMap => {
+      const id = +parameterMap.get('id');
+      this.getPet(id);
+    })
+
+  }
+  private getPet(id) {
+    if (id === 0) {
+      this.pet = {
+        id: null,
+        cId: null,
+        petName: '',
+        birthDate: '',
+        weight: null,
+        type: null,
+        breed: '',
+        description: '' 
+      };
+      this.panelTitle = 'Create Pet';
+      // this.createPetForm.resetForm();
+    } else {
+      // this.pet = Object.assign({}, this._petService.getPet(id));
+      this.aniService.getPet(id).subscribe(
+        (pet) => this.pet = pet,
+        (err: any) => console.log('create-pet.comp:' + err)
+      );
+      this.panelTitle = 'Edit Details';
+    }
+  }
+   savePet(): void { 
+    // const newPet: Pet = Object.assign ({}, this.pet); //no longer worry about addressing reference var
+    //this._petService.save(newPet)(
+      // this._petService.save(this.pet).subscribe(
+      if (this.pet.id === null) {
+        this.aniService.addPet(this.pet).subscribe(
+          (data: Pet) => {
+            console.log(data);
+            // this.createPetForm.resetForm();
+            // this.activatedRoute.navigate(['/']);
+          },
+          (error: any) => console.log(error)
+        );
+      } else {
+        this.aniService.updatePet(this.pet).subscribe(
+          () => { 
+            // this.createPetForm.reset();
+            // this.activatedRoute.navigate(['/']);
+          },
+          (error: any) => console.log(error)
+        );
+      } 
+   
+  }
+  // pets_url: string = 'http://localhost:8080/pets';
+  // [{
+  // 	"id": 2,
+  // 	"cId": 1,
+  // 	"petName": "Whiskers",
+  // 	"birthDate": "2017-08-01 00:00:00.0",
+  // 	"weight": 69,
+  // 	"type": 2,
+  // 	"breed": "English Bdog",
+  // 	"description": null
+  // }]
+
 }
