d00d6acbe10caa007faede9ce47314935ac89739
Thomas M. Maestas <thomasm1.maestas@gmail.com>
2020-01-18 02:48:14 -0500

    Brendan branch (#48)
    * adds Pet Model
    
    * adds Pet Repository
    
    * adds pet service interface
    
    * updates C_id to cid to work with CRUD Repo
    
    * adds get pets by CID method
    
    * adds pet by CID method
    
    * adds Pet Service Impl
    
    * adds Pet Controller
    
    * adds references/annotations
    
    * updates method name to proper entity id
    
    * updates c_id methods name
    
    * fixes typo in description
    
    * doggywood angular-DB connection: vetPetProfile
    
    * removes info
    
    * updates model to reflect changes
    
    * updates for Angular CORS
    
    * all methods working with exception of pet by CID
    
    * updates pet model to reflect DB
    
    * updates delet method
    
    * updates input to petbyCID
    
    * updates gitingore to remove Servers and app properties
    
    * minor changes
    
    * adds cutomer CRUD repository
    
    * adds Customer Service inteface
    
    * adds Customer Service Impl
    
    * adds Customer Controller
    
    * addes Employee entitie
    
    * adds employee repository
    
    * adds Employee Service interface
    
    * adds Employee Service Impl
    
    * adds @Service annotoation
    
    Co-authored-by: flubbid <49663994+flubbid@users.noreply.github.com>
    Co-authored-by: daviscowles <53130319+daviscowles@users.noreply.github.com>

---
+++ BackEnd_Master/src/main/java/com/doggywood/services/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.doggywood.services;
+
+import java.util.List;
+
+import com.doggywood.entities.Employee;
+
+
+public interface EmployeeService {
+
+	public Employee createEmployee(Employee employee);
+	public Employee getEmployeeById(int id);
+	public List<Employee> getAllEmployees();
+	public Employee updatesEmployee(Employee change);
+	public boolean deleteEmployee(Employee employee);
+	
+}
