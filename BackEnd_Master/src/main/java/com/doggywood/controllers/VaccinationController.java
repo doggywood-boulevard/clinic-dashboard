e604dff520e996de374c9da9e8354d33467394ab
Ruben Colon <Kiketon@Rubens-MacBook-Pro.local>
2020-01-18 19:27:01 -0500

    add all layers for Vaccination and Appointment tables

---
+++ BackEnd_Master/src/main/java/com/doggywood/controllers/VaccinationController.java
@@ -0,0 +1,28 @@
+package com.doggywood.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.doggywood.entities.Vaccination;
+import com.doggywood.services.VaccinationService;
+
+@RestController
+public class VaccinationController {
+
+	@Autowired
+	VaccinationService vs;
+	
+	@GetMapping(value = "/vaccinations")
+	public List<Vaccination> getAllVaccinations() {
+		return vs.getAllVaccinations();
+	}
+	
+	@GetMapping(value = "/vaccinations/{id}")
+	public Vaccination getVaccinationById(@PathVariable("id") int id) {
+		return vs.getVaccinationById(id);
+	}
+}
