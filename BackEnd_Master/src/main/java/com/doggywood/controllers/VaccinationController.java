df7712de1498d5fb1d3e2fd76cf0d809d0107257
Ruben Colon <Kiketon@Rubens-MacBook-Pro.local>
2020-01-20 20:58:08 -0500

    add model and service for appointment in angular

--- BackEnd_Master/src/main/java/com/doggywood/controllers/VaccinationController.java
+++ BackEnd_Master/src/main/java/com/doggywood/controllers/VaccinationController.java
@@ -3,6 +3,7 @@ package com.doggywood.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.doggywood.entities.Vaccination;
 import com.doggywood.services.VaccinationService;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class VaccinationController {
 
