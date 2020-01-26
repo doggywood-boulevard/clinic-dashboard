35eab02bd8397dc8de98f77165f1d6c0e130aaf2
flubbid <brendanwilson1@gmail.com>
2020-01-26 15:52:25 -0500

    adds logic for vac-create

--- doggywood/src/app/components/vac-create/vac-create.component.ts
+++ doggywood/src/app/components/vac-create/vac-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { VaccRecordService } from 'src/app/services/vacc-record.service';
+import { PetsService } from 'src/app/services/pets.service';
+import { Pet } from 'src/app/models/pet';
 
 @Component({
   selector: 'app-vac-create',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VacCreateComponent implements OnInit {
 
-  constructor() { }
+  int: number;
+  petId: number;
+  vName: string;
+  vTime: number;
+  vDate: string;
+  pet: Pet;
+
+
+  constructor(private recordService :VaccRecordService, private petService :PetsService) { }
 
   ngOnInit() {
   }
+
+addRecord(){
+
+
+}
+
+
+getPet(){
+this.petService.getPet(this.petId).subscribe(
+  response => {
+    this.pet = response;
+  },
+  response => {
+    console.log("Failed to get pets")
+  }
+
+)
+
+}
 
 }
