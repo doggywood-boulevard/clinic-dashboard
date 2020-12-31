a96c0128be8768b5fc00b566fc81374d34a2c688
thomas maestas <thomasm1.maestas@gmail.com>
2020-12-30 22:24:41 -0500

    added TestNG-Selenium for home, login, secureArea pages

---
+++ BackEnd_Master/src/main/java/com/doggywood/selenium/pages/SecureAreaPage.java
@@ -0,0 +1,35 @@
+package com.doggywood.selenium.pages;
+
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class SecureAreaPage extends BasePageObject {
+
+	private String pageUrl = "http://localhost:4200/clients/1";
+//	private String pageUrl = "http://localhost:4200/login";
+ 
+//	private By logOutButton = By.xpath("//a[@class='logout']");
+	private By logOutButton = By.id("logout");
+	private By message = By.id("flash-messages");
+
+	public SecureAreaPage(WebDriver driver, Logger log) {
+		super(driver, log);
+	}
+
+	/** Get URL variable from PageObject */
+	public String getPageUrl() {
+		return pageUrl;
+	}
+
+	/** Verification if logOutButton is visible on the page */
+	public boolean isLogOutButtonVisible() {
+		return find(logOutButton).isDisplayed();
+	}
+
+	/** Return text from success message */
+	public String getSuccessMessageText() {
+		return find(message).getText();
+	}
+
+}
